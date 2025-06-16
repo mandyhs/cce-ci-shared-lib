2c0d56bf53d9a77533c293807e56ea826d694c57
Hsieh, Mandy <mandy.hsieh@intel.com>
2025-06-16 15:48:42 +0800

    update report

--- src/org/example/ci/ReportGenerator.groovy
+++ src/org/example/ci/ReportGenerator.groovy
@@ -7,14 +7,30 @@ class ReportGenerator implements Serializable {
         this.script = script
     }
 
-    def generateTxtReport(params) {
-        def commit = script.sh(script: "git rev-parse --short HEAD", returnStdout: true).trim()
+    def generateTxtReport(params, List<Map> reposConfig) {
+        script.echo "Generating CI report..."
+        if (!params || !reposConfig) {
+            script.error "Parameters or repository configuration is missing. Cannot generate report."
+            return
+        }
+        def commits = []
+        reposConfig.eachWithIndex { repo, index ->
+            script.echo "--- Processing Repository ${index + 1} ---"
+            def repoTargetDir = repo.targetDir
+            script.dir(repoTargetDir) {
+                def commit = script.powershell(script: "git rev-parse --short HEAD", returnStdout: true).trim()
+                commits.add("Repository ${index + 1} (${repo.url}): ${commit}")
+                script.echo "Current commit for ${repoTargetDir}: ${commit}"
+            }
+        }
+
+        
         def content = """
         CI Report
         --------------------------
         Build #: ${script.env.BUILD_NUMBER}
         Branch : ${script.env.BRANCH_NAME ?: 'N/A'}
-        Commit : ${commit}
+        Commit : ${commits.join('\n')}
         Target : ${params.TARGET}
         Env    : ${params.ENVIRONMENT}
         Result : ${script.currentBuild.currentResult}
