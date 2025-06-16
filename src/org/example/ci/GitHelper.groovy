2096b7ef0b36738b9feff43afd20e514ea04d860
Hsieh, Mandy <mandy.hsieh@intel.com>
2025-06-16 14:27:16 +0800

    update scm

--- src/org/example/ci/GitHelper.groovy
+++ src/org/example/ci/GitHelper.groovy
@@ -17,9 +17,20 @@ class GitHelper implements Serializable {
             def dirName = repo.tokenize('/').last().replaceAll('.git$', '')
             script.dir(dirName) {
                 echo "Checking out ${repo} on branch ${branch}"
-                script.checkout([$class: 'GitSCM',
+                /*script.checkout([$class: 'GitSCM',
                     branches: [[name: branch]],
                     userRemoteConfigs: [[url: repo]]
+                ])*/
+
+                script.checkout([$class: 'GitSCM', branches: [[name: "*/${branch}"]], 
+                  extensions: [[$class: 'LocalBranch', localBranch: "${branch}"],[$class: 'GitLFSPull'],
+                  [$class: 'DisableRemotePoll'],
+                  [$class: 'CheckoutOption', timeout: 30], 
+                  [$class: 'CloneOption', noTags: false, reference: '', shallow: false, timeout: 30],
+                  [$class: 'CleanCheckout', deleteUntrackedNestedRepositories: true], [$class: 'CleanBeforeCheckout', deleteUntrackedNestedRepositories: true],  
+                  [$class: 'RelativeTargetDirectory', relativeTargetDir: 'ice_source']], 
+                  gitTool: 'jgit', userRemoteConfigs: [[credentialsId: '746ebc8c-18cd-43aa-a425-a14134b6beee', url: repo]],
+                  changelog: true, poll: true
                 ])
             }
         }
